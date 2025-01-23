efc60184cb0a629cb7a10eca8df5b1bea5b999d6
wazed biplob <www.biplob@gmail.com>
2025-01-23 18:04:50 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,26 @@
 import express from "express";
-export const app = express();
+import cors from "cors";
+const app = express();
 const port = 5000;
+
+app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://wb-codebase.vercel.app",
+];
+app.use(
+  cors({
+    origin: (origin: any, callback: any) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "DELETE", "UPDATE"],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -9,3 +29,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+export default app;
